3a3ed5dcbb28abf8bfc70a8243931fe3f155b886
Abdullah Omar <abdullahomar.dev@gmail.com>
2023-09-25 23:43:37 -0700

    add dummy data for testing

---
+++ dummyData.ts
@@ -0,0 +1,72 @@
+export const products = [
+    {
+      id: 1,
+      name: 'Basic Tee',
+      description: "لقد أصبحنا وحدنا أخيرا .. أنا وأنت .. ‏أخيرا انفردت بك .. ‏وصرت أملكك .. وأملك عينيك .. في كل مرة تنظر فيها إلى كلماتي .. و تقرأ فيها سطوري .. ‏ستكون هذه هي آخر رواية ستقرأها لي في حياتك .. فأنا على شفا حفرة من الموت .. ‏ولم يتبق لي في هذه الحياة إلا سويعات لا أدري عددها .. لكنني أعرف أنها قليلة .. وبرغم ذلك فهي كافية لأسقيك بما أريد أن أسقيك إياه من ‏الحديث.‏ قبل أن أبدأ أقول لك .. يجب أن تقرأ هذا الكتاب وتحرقه .. فسيحاولون التخلص منه ومن كل من قرأه .. كما فعلوا مع كل الكتب التي شابهته." ,   
+      href: '#',
+      imageSrc: 'https://cdn.aseeralkotb.com/storage/media/320208/conversions/%D8%A3%D9%86%D8%AA%D9%8A%D8%AE%D8%B1%D9%8A%D8%B3%D8%AA%D9%88%D8%B3-125-200x300-webp.webp',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: '$35',
+      discount: 20 ,
+      color: 'Black',
+      author: 'Ahmed Khaled Mustafa' ,
+      pages: 320 ,
+      publicationYear: 2017 ,
+      categories: ['arabic novels' , 'historical fiction'] ,
+      ISBN: '9789778515671' ,
+      rating: {
+        value: 4.5/5 ,
+        number: 173
+      }
+  
+    },
+    {
+      id: 2,
+      name: 'Basic Tee',
+      href: '#',
+      imageSrc: 'https://cdn.aseeralkotb.com/storage/media/320208/conversions/%D8%A3%D9%86%D8%AA%D9%8A%D8%AE%D8%B1%D9%8A%D8%B3%D8%AA%D9%88%D8%B3-125-200x300-webp.webp',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: '$35',
+      color: 'Black',
+    },
+    {
+      id: 3,
+      name: 'Basic Tee',
+      href: '#',
+      imageSrc: 'https://cdn.aseeralkotb.com/storage/media/320208/conversions/%D8%A3%D9%86%D8%AA%D9%8A%D8%AE%D8%B1%D9%8A%D8%B3%D8%AA%D9%88%D8%B3-125-200x300-webp.webp',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: '$35',
+      color: 'Black',
+    },
+    {
+      id: 4,
+      name: 'Basic Tee',
+      href: '#',
+      imageSrc: 'https://cdn.aseeralkotb.com/storage/media/320208/conversions/%D8%A3%D9%86%D8%AA%D9%8A%D8%AE%D8%B1%D9%8A%D8%B3%D8%AA%D9%88%D8%B3-125-200x300-webp.webp',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: '$35',
+      color: 'Black',
+    },
+    {
+      id: 5,
+      name: 'Basic Tee',
+      href: '#',
+      imageSrc: 'https://cdn.aseeralkotb.com/storage/media/320208/conversions/%D8%A3%D9%86%D8%AA%D9%8A%D8%AE%D8%B1%D9%8A%D8%B3%D8%AA%D9%88%D8%B3-125-200x300-webp.webp',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: '$35',
+      color: 'Black',
+    },
+    // More products...
+  ]
+
+
+export const authors = [
+    {
+        id: '1' ,
+        name: 'Ahmed Khaled Mustafa' ,
+        bio: 'من مواليد المدينة المنورة بالمملكة العربية السعودية في 22 يناير 1984م..درس الصيدلة في جامعة القاهرة، ثم اتجه بعدها للكتابة حيث كانت قصة مطعم اللحوم البشرية أول أعماله، تلاها روايات أنتيخريستوس وأرض السافلين، وملائك نصيبين هي أحدث أعماله الصادرة جميعاً عن دار عصير الكتب للنشر والتوزيع، وقد عرفت أعماله بكونها مزيجا من المعلومات التاريخية والدينية والخيال..',
+        books: [
+            
+        ]
+    }
+]
